5c304e4fd18571c698010f8d303a23dd17c38cb3
ashirbad behera <ashirbadbehera29@gmail.com>
2021-02-28 22:52:54 +0530

    landing section complete

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Landing.css';
+import Typewriter from 'typewriter-effect';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import hero from '../../assets/hero.png';
 
 const Landing = () => {
+	useEffect(() => {
+		AOS.init();
+		AOS.refresh();
+	}, []);
+
 	return (
 		<div className='landing-container'>
-			<div className='landing-left'>
+			<div data-aos='fade-right' className='landing-left'>
 				<h1 className='landing-header'>Can You Type</h1>
 				<div className='typewriter-container'>
-					<div>Fast?</div>
-					<div>Correct?</div>
-					<div>Quick?</div>
+					<Typewriter
+						options={{
+							strings: ['Fast', 'Quick', 'Correct'],
+							autoStart: true,
+							loop: true,
+						}}
+					/>
 				</div>
 			</div>
-			<div className='landing-right'>
+			<div data-aos='fade-left' className='landing-right'>
 				<img src={hero} alt='hero' className='flash-image' />
 			</div>
 		</div>
